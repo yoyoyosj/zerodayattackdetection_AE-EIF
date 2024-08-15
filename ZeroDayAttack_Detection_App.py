3f7efeddd4d6e7923baae5d3facccf266af55b60
yoyoyosj <140308847+yoyoyosj@users.noreply.github.com>
2024-08-16 00:36:35 +0800

    Update ZeroDayAttack_Detection_App.py

--- ZeroDayAttack_Detection_App.py
+++ ZeroDayAttack_Detection_App.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import pickle
 import streamlit as st
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import plotly.express as px
 import requests
